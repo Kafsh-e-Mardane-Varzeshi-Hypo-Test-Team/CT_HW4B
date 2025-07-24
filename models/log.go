921b4d132f5d6ffb3fa5beb42854dacb67650f49
mammedbrk <mammedbrk@gmail.com>
2025-07-25 02:57:03 +0330

    refactor: replace keys []string with data map[string]string in LogPayload and Event structs

--- models/log.go
+++ models/log.go
@@ -7,10 +7,10 @@ import (
 )
 
 type LogPayload struct {
-	Name      string    `json:"name" binding:"required"`
-	Timestamp time.Time `json:"timestamp" binding:"required"`
-	Keys      []string  `json:"keys" binding:"required"`
-	TTL       *int64    `json:"ttl,omitempty"`
+	Name      string            `json:"name" binding:"required"`
+	Timestamp time.Time         `json:"timestamp" binding:"required"`
+	Data      map[string]string `json:"data" binding:"required"`
+	TTL       *int64            `json:"ttl,omitempty"`
 }
 
 type LogRequest struct {
@@ -22,13 +22,13 @@ type LogRequest struct {
 
 // Event represents a log event stored in the database
 type Event struct {
-	EventID   gocql.UUID `json:"event_id"`
-	ProjectID string     `json:"project_id"`
-	Name      string     `json:"name"`
-	Timestamp time.Time  `json:"timestamp"`
-	Keys      []string   `json:"keys"`
-	CreatedAt time.Time  `json:"created_at"`
-	TTL       int64      `json:"ttl"`
+	EventID   gocql.UUID        `json:"event_id"`
+	ProjectID string            `json:"project_id"`
+	Name      string            `json:"name"`
+	Timestamp time.Time         `json:"timestamp"`
+	Data      map[string]string `json:"data"`
+	CreatedAt time.Time         `json:"created_at"`
+	TTL       int64             `json:"ttl"`
 }
 
 // EventSummary represents aggregated event data for the frontend
